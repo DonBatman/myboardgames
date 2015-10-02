21cf4bced71303db8a226fbce06418dca9e12829
DonBatman <serfdon@gmail.com>
2015-10-01 20:51:07 -0700

    added mazes and diamond block

---
+++ mydiamondblock/init.lua
@@ -0,0 +1,37 @@
+minetest.register_node("mydiamondblock:block",{
+	description = "My Diamond Block",
+	inventory_image = "mydiamondblock.png",
+	tiles = {"mydiamondblock.png"},
+	drawtype = "normal",
+	paramtype = "light",
+	light_source = 8,
+	groups = {cracky = 1},
+on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+local schem = minetest.get_modpath("mydiamondblock").."/schems/mydiamondblock.mts"
+	minetest.place_schematic(pos,schem,0, "air", true)
+end,
+})
+for blues = 1,6 do
+local blue = blues
+minetest.register_node("mydiamondblock:blue"..blue,{
+	description = "Blue "..blue,
+	inventory_image = "mydiamondblock_blue"..blue..".png",
+	tiles = {"mydiamondblock_blue"..blue..".png"},
+	drawtype = "normal",
+	paramtype = "light",
+	light_source = 8,
+	groups = {cracky = 1},
+})
+end
+for browns = 1,4 do
+local brown = browns
+minetest.register_node("mydiamondblock:brown"..brown,{
+	description = "Brown "..brown,
+	inventory_image = "mydiamondblock_brown"..brown..".png",
+	tiles = {"mydiamondblock_brown"..brown..".png"},
+	drawtype = "normal",
+	paramtype = "light",
+	light_source = 8,
+	groups = {cracky = 1},
+})
+end
---
+++ mymazes/init.lua
@@ -0,0 +1,26 @@
+minetest.register_node("mymazes:block1",{
+	description = "Maze 1",
+	inventory_image = "mymazes_1.png",
+	tiles = {"mymazes_1.png"},
+	drawtype = "normal",
+	paramtype = "light",
+	light_source = 8,
+	groups = {cracky = 1},
+on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+local schem = minetest.get_modpath("mymazes").."/schems/mymaze1.mts"
+	minetest.place_schematic(pos,schem,0, "air", true)
+end,
+})
+minetest.register_node("mymazes:block2",{
+	description = "Maze 2",
+	inventory_image = "mymazes_2.png",
+	tiles = {"mymazes_2.png"},
+	drawtype = "normal",
+	paramtype = "light",
+	light_source = 8,
+	groups = {cracky = 1},
+on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+local schem = minetest.get_modpath("mymazes").."/schems/mymaze2.mts"
+	minetest.place_schematic(pos,schem,0, "air", true)
+end,
+})
