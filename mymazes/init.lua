f0ad3c945093f7ad8922b338069acbc3181988f9
Don <serfdon@gmail.com>
2025-11-24 09:34:53 -0800

    Code Update

--- mymazes/init.lua
+++ mymazes/init.lua
@@ -1,4 +1,4 @@
-minetest.register_node("mymazes:block1",{
+core.register_node("mymazes:block1",{
 	description = "Maze 1",
 	inventory_image = "mymazes_1.png",
 	wield_image = "mymazes_1.png",
@@ -8,18 +8,18 @@ minetest.register_node("mymazes:block1",{
 	light_source = 8,
 	groups = {cracky = 1},
 	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-	local schem = minetest.get_modpath("mymazes").."/schems/mymaze1.mts"
-		minetest.place_schematic(pos,schem,0, "air", true)
+	local schem = core.get_modpath("mymazes").."/schems/mymaze1.mts"
+		core.place_schematic(pos,schem,0, "air", true)
 	end,
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		if placer and minetest.check_player_privs(placer:get_player_name(), {myboardgames = true}) then
+		if placer and core.check_player_privs(placer:get_player_name(), {myboardgames = true}) then
 		else
-			minetest.remove_node(pos)
+			core.remove_node(pos)
 			return true
 		end
 	end,
 })
-minetest.register_node("mymazes:block2",{
+core.register_node("mymazes:block2",{
 	description = "Maze 2",
 	inventory_image = "mymazes_2.png",
 	wield_image = "mymazes_2.png",
@@ -29,13 +29,13 @@ minetest.register_node("mymazes:block2",{
 	light_source = 8,
 	groups = {cracky = 1},
 	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-	local schem = minetest.get_modpath("mymazes").."/schems/mymaze2.mts"	
-		minetest.place_schematic(pos,schem,0, "air", true)
+	local schem = core.get_modpath("mymazes").."/schems/mymaze2.mts"	
+		core.place_schematic(pos,schem,0, "air", true)
 	end,
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		if placer and minetest.check_player_privs(placer:get_player_name(), {myboardgames = true}) then
+		if placer and core.check_player_privs(placer:get_player_name(), {myboardgames = true}) then
 		else
-			minetest.remove_node(pos)
+			core.remove_node(pos)
 			return true
 		end
 	end,
