279aa4b94025ba5bbbbb378cb4f695c90fcd9c50
Don <serfdon@gmail.com>
2025-11-24 09:30:26 -0800

    Code Update

--- mycheckers/init.lua
+++ mycheckers/init.lua
@@ -1,5 +1,5 @@
 
-minetest.register_node("mycheckers:board", {
+core.register_node("mycheckers:board", {
 	description = "Checkers",
 	inventory_image = "mycheckers_inv.png",
 	wield_image = "mycheckers_inv.png",
@@ -11,13 +11,13 @@ minetest.register_node("mycheckers:board", {
 	sounds = default.node_sound_stone_defaults(),
 
 	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-	local schem = minetest.get_modpath("mycheckers").."/schems/mycheckersboard.mts"
-		minetest.place_schematic({x=pos.x,y=pos.y,z=pos.z},schem,0, "air", true)
+	local schem = core.get_modpath("mycheckers").."/schems/mycheckersboard.mts"
+		core.place_schematic({x=pos.x,y=pos.y,z=pos.z},schem,0, "air", true)
 	end,
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		if placer and minetest.check_player_privs(placer:get_player_name(), {myboardgames = true}) then
+		if placer and core.check_player_privs(placer:get_player_name(), {myboardgames = true}) then
 		else
-			minetest.remove_node(pos)
+			core.remove_node(pos)
 			return true
 		end
 	end,
@@ -60,7 +60,7 @@ for i in ipairs (board) do
 local item = board[i][1]
 local img = board[i][2]
 
-minetest.register_node(item, {
+core.register_node(item, {
 	description = item,
 	light_source = 5,
 	tiles = img,
@@ -91,7 +91,7 @@ local crown = {
 		}
 	}
 
-minetest.register_node("mycheckers:redp_c", {
+core.register_node("mycheckers:redp_c", {
 	description = "Red Crown",
 	light_source = 5,
 	tiles = {"default_acacia_wood.png"},
@@ -102,7 +102,7 @@ minetest.register_node("mycheckers:redp_c", {
 	groups = {cracky = 1, dig_immediate=3,not_in_creative_inventory=1},
 	node_box = crown,
 })
-minetest.register_node("mycheckers:blackp_c", {
+core.register_node("mycheckers:blackp_c", {
 	description = "Black Crown",
 	light_source = 5,
 	tiles = {"default_obsidian.png"},
@@ -113,7 +113,7 @@ minetest.register_node("mycheckers:blackp_c", {
 	groups = {cracky = 1, dig_immediate=3,not_in_creative_inventory=1},
 	node_box = crown,
 })
-minetest.register_node("mycheckers:blackp", {
+core.register_node("mycheckers:blackp", {
 	description = "Black Piece",
 	light_source = 5,
 	tiles = {"default_obsidian.png"},
@@ -123,19 +123,19 @@ minetest.register_node("mycheckers:blackp", {
 	groups = {cracky = 1, dig_immediate=3,not_in_creative_inventory=1},
 	node_box = regp,
 after_place_node = function(pos, placer, itemstack, pointed_thing)
-	local node = minetest.get_node(pos)
-	local nodeu = minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z})
+	local node = core.get_node(pos)
+	local nodeu = core.get_node({x=pos.x,y=pos.y-1,z=pos.z})
 	if nodeu.name == "mycheckers:blackp" and
 	   node.name == "mycheckers:blackp" then
-	   minetest.set_node({x=pos.x,y=pos.y-1,z=pos.z},{name = "mycheckers:blackp_c"})
-	   minetest.set_node(pos,{name = "air"})
+	   core.set_node({x=pos.x,y=pos.y-1,z=pos.z},{name = "mycheckers:blackp_c"})
+	   core.set_node(pos,{name = "air"})
 	elseif nodeu ~= "mycheckers:blackp" then
-		minetest.set_node(pos,{name="mycheckers:blackp"})
+		core.set_node(pos,{name="mycheckers:blackp"})
 	end
 	
 end
 })
-minetest.register_node("mycheckers:redp", {
+core.register_node("mycheckers:redp", {
 	description = "Red Piece",
 	light_source = 5,
 	tiles = {"default_acacia_wood.png"},
@@ -145,15 +145,15 @@ minetest.register_node("mycheckers:redp", {
 	groups = {cracky = 1, dig_immediate=3,not_in_creative_inventory=1},
 	node_box = regp,
 after_place_node = function(pos, placer, itemstack, pointed_thing)
-	local node = minetest.get_node(pos)
-	local nodeu = minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z})
+	local node = core.get_node(pos)
+	local nodeu = core.get_node({x=pos.x,y=pos.y-1,z=pos.z})
 	
 	if 	nodeu.name == "mycheckers:redp" and
 	   	node.name == "mycheckers:redp" then
-	   	minetest.set_node({x=pos.x,y=pos.y-1,z=pos.z},{name = "mycheckers:redp_c"})
-	   	minetest.set_node(pos,{name = "air"})
+	   	core.set_node({x=pos.x,y=pos.y-1,z=pos.z},{name = "mycheckers:redp_c"})
+	   	core.set_node(pos,{name = "air"})
 	elseif nodeu ~= "mycheckers:redp" then
-		minetest.set_node(pos,{name="mycheckers:redp"})
+		core.set_node(pos,{name="mycheckers:redp"})
 	end
 	
 end
